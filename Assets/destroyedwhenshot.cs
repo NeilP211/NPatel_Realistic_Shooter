8c30f3bb6d946f52b82a889ade2e77c2d90ac215
FM Orcaz_YT <55113398+MeteoricOrcasYT@users.noreply.github.com>
2020-05-09 11:57:55 -0400

    5/9/2020
    We fixed all the bugs including the camera glitch and the gun bobbing one. We started to work on our animated targets.

---
+++ Assets/destroyedwhenshot.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyedwhenshot : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "thetag")
+            Destroy(gameObject);
+    }
+}
