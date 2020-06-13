3a7545e3945cfea59977be5a42fbc554cbd7a4be
FM Orcaz_YT <55113398+MeteoricOrcasYT@users.noreply.github.com>
2020-06-13 11:59:34 -0400

    6/13/2020
    Fixed Movement of the Bandit and made it look at the player from a certain distance

---
+++ Assets/HeadTurn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadTurn : MonoBehaviour
+{
+    public Transform Head;
+    public Transform player;
+    public float Distance_;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Distance_ = Vector3.Distance(Head.transform.position, player.transform.position);
+        if (Distance_ <= 5)
+        {
+            transform.LookAt(player);
+        }
+            
+    }
+}
