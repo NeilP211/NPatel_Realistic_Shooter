2d9041c81da2fa517d7d0ddede5657d6cce1b102
FM Orcaz_YT <55113398+MeteoricOrcasYT@users.noreply.github.com>
2020-06-20 11:58:44 -0400

    6/20/2020
    We started working on the transitions

--- Assets/Scripts/HeadTurn.cs
+++ Assets/Scripts/HeadTurn.cs
@@ -7,20 +7,25 @@ public class HeadTurn : MonoBehaviour
     public Transform Head;
     public Transform player;
     public float Distance_;
-        
+    Animator animator;
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        animator.SetFloat("Distance",Distance_);
         Distance_ = Vector3.Distance(Head.transform.position, player.transform.position);
         if (Distance_ <= 5)
         {
             transform.LookAt(player);
+            rb.AddForce(transform.forward * 10000);
         }
             
     }
